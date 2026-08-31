2a880076d756796451bbf03ba03be8457bb05520
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-31 21:31:37 +0100

    Refresh Evia speech timing patch

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='evia-pwa-v19';
+const C='evia-pwa-v20';
 const UPDATE_UI_MARKER='evia-update-ui-ready-v1';
 const RELEASE_VERSION='1.0';
 const RELEASE_MARKER_URL=new URL('./__evia-visible-release-version__',self.registration.scope).href;
