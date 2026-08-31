4ba89595909df537839283ce016781d7de5478ce
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-31 16:48:40 +0100

    Load approved Evia EPA plan

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const C='evia-pwa-v6';
-const F=['./manifest.webmanifest','./evia-approved-features.js','./evia-approved-learning-ui.js','./evia-approved-menu-support.js','./icons/evia-180.png','./icons/evia-192.png','./icons/evia-512.png'];
+const C='evia-pwa-v7';
+const F=['./manifest.webmanifest','./evia-approved-features.js','./evia-approved-learning-ui.js','./evia-approved-menu-support.js','./evia-approved-epa.js','./icons/evia-180.png','./icons/evia-192.png','./icons/evia-512.png'];
 const QR_CACHE='evia-feature-lib-v1';
 const QR_LIBRARY_URL='https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js';
 const NAXOS_CACHE='evia-naxos-offline-v1';
@@ -18,6 +18,7 @@ function injectFeatures(html){
   if(!html.includes('evia-approved-features.js'))tags.push('<script src="./evia-approved-features.js"></script>');
   if(!html.includes('evia-approved-learning-ui.js'))tags.push('<script src="./evia-approved-learning-ui.js"></script>');
   if(!html.includes('evia-approved-menu-support.js'))tags.push('<script src="./evia-approved-menu-support.js"></script>');
+  if(!html.includes('evia-approved-epa.js'))tags.push('<script src="./evia-approved-epa.js"></script>');
   if(!tags.length)return html;
   const tag=tags.join('');
   return html.includes('</body>')?html.replace('</body>',`${tag}</body>`):`${html}${tag}`;
