0e2c22bb5ab2f8dba0e8fe1e6e692189dbcdfb82
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-01 15:22:38 +0100

    Deploy full Evia demo tour update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const C='evia-pwa-v32';
+const C='evia-pwa-v33';
 const UPDATE_UI_MARKER='evia-update-ui-ready-v1';
 const RELEASE_VERSION='1.0';
 const RELEASE_MARKER_URL=new URL('./__evia-visible-release-version__',self.registration.scope).href;
