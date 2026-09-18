872672600d58b206804001c4ed746035bdd13ac4
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-09-18 22:16:02 +0100

    Restore persistent opening Evia screen

--- profile.js
+++ profile.js
@@ -94,7 +94,7 @@
 
   function welcome(){
     const p=get();
-    const root=document.createElement("div");root.id="welcome-screen";
+    const root=document.createElement("div");root.id="welcome-screen";root.style.opacity="1";root.style.zIndex="2000";
     root.innerHTML='<div class="welcome-inner">'+
       '<div class="welcome-avatar evia-welcome-face"><span class="evia-face"><i></i><i></i></span></div>'+
       '<div class="welcome-pulse"></div>'+
